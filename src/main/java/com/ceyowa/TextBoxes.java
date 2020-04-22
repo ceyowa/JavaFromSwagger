463f3af25632631a0c81dd6dbd079b2de5b4e2c5
ChenYouhua <chenyouhua@spaceon.com>
2020-04-22 17:33:34 +0800

    改用本地的Idea

--- src/main/java/com/ceyowa/TextBoxes.java
+++ src/main/java/com/ceyowa/TextBoxes.java
@@ -1,4 +1,10 @@
-package com.ceyowa
+package com.ceyowa;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 
 public class TextBoxes extends AnAction {
 
